b64957cea3b4096a11765d24b5515ad9d39a4674
Robin Chen <robin.chen@flippengroup.com>
2017-10-22 18:12:12 -0500

    convert to pcl profile 111 library

--- Dates.Recurring/Enums/Month.cs
+++ Dates.Recurring/Enums/Month.cs
@@ -10,29 +10,28 @@ namespace Dates.Recurring
     [Flags]
     public enum Month : int
     {
-        [Description("January")]
         JANUARY = 1,
-        [Description("February")]
+
         FEBRUARY = 2,
-        [Description("March")]
+
         MARCH = 4,
-        [Description("April")]
+
         APRIL = 8,
-        [Description("May")]
+
         MAY = 16,
-        [Description("June")]
+
         JUNE = 32,
-        [Description("July")]
+
         JULY = 64,
-        [Description("August")]
+
         AUGUST = 128,
-        [Description("September")]
+
         SEPTEMBER = 256,
-        [Description("October")]
+
         OCTOBER = 512,
-        [Description("November")]
+
         NOVEMBER = 1024,
-        [Description("December")]
+
         DECEMBER = 2048
     }
 }
--- Dates.Recurring/Properties/AssemblyInfo.cs
+++ Dates.Recurring/Properties/AssemblyInfo.cs
@@ -17,10 +17,8 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5b9e574d-92a0-452a-809a-51d96b697e82")]
 
 // Version information for an assembly consists of the following four values:
 //
